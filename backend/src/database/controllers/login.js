24139106294e9df1d4d9a13ba96d01d8eb6f86bf
EmilyNilsen <nilsen.loli.emily@gmail.com>
2022-08-08 20:13:38 -0300

    feat:  endpoint response fix

--- backend/src/database/controllers/login.js
+++ backend/src/database/controllers/login.js
@@ -4,7 +4,7 @@ const LoginController = async (req, res) => {
   const { email, password } = req.body;
   const response = await userService.login({ email, password });
   if (!response) return res.status(404).json({ message: 'User not found'})
-  return res.status(200).json({ message: 'Login feito com sucesso!'})
+  return res.status(200).send();
 }
 
 module.exports = LoginController;
