f28f0bf2815bf360519c922bc86e8eec6197890c
EmilyNilsen <nilsen.loli.emily@gmail.com>
2022-08-08 22:17:13 -0300

    fix

--- backend/src/database/controllers/contacts.js
+++ backend/src/database/controllers/contacts.js
@@ -1,10 +1,14 @@
 const contactService = require('../services/contacts');
 
 const listContactsByUserId = async (req, res) => {
-  const { id } = req.params;
-  const response = await contactService.listContactsByUserId({ id });
-  if (!response) return res.status(404).json({ message: 'Contacts not found'})
-  return res.status(200).json(response);
+  try {
+    const { id } = req.params;
+    const response = await contactService.listContactsByUserId({ id });
+    return res.status(200).json(response);
+  } catch (e) {
+    console.error(e);
+    return res.status(400);
+  }
 }
 
 module.exports = listContactsByUserId;
