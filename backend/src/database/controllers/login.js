afa02f6cd772f79c3630e124886dc60b5e5adb35
Emily Nilsen Carvalho <84545237+EmilyNilsen@users.noreply.github.com>
2022-08-11 00:02:25 -0300

    fix: translate error messages

--- backend/src/database/controllers/login.js
+++ backend/src/database/controllers/login.js
@@ -3,7 +3,7 @@ const userService = require('../services/login');
 const LoginController = async (req, res) => {
   const { email, password } = req.body;
   const response = await userService.authentication({ email, password });
-  if (!response) return res.status(404).json({ message: 'User not found'})
+  if (!response) return res.status(404).json({ message: 'Usuário não encontrado'})
   return res.status(200).json(response);
 }
 
